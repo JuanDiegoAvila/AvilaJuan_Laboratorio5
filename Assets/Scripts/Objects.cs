3fc6e7e4f68458974d0ab7fb4aad7faaa13f9e67
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2021-02-17 13:24:48 -0600

    primer commit

---
+++ Assets/Scripts/Objects.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objects : MonoBehaviour
+{
+    AudioSource press;
+    public float force = 0.0f;
+    Rigidbody rb;
+
+    
+    MeshRenderer mesh;
+    Color original;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        press = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
+        mesh = GetComponent<MeshRenderer>();
+
+
+        original = mesh.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+
+        mesh.material.color = Color.red;
+    }
+
+    private void OnMouseExit()
+    {
+        mesh.material.color = original;
+    }
+
+    private void OnMouseDown()
+    {
+        
+        RaycastHit hitInfo;
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+       Physics.Raycast(camRay, out hitInfo);
+        press.Play();
+        rb.AddForce(-hitInfo.normal * 5, ForceMode.Impulse);
+    }
+
+}
---
+++ Assets/Scripts/camRay.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class camRay : MonoBehaviour
+{
+
+    public GameObject explosion;
+
+    AudioSource audioSource;
+    public AudioClip destroy;
+    public AudioClip shot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    
+
+    public void Explode(Vector3 position)
+    {
+        Instantiate(explosion, position, Quaternion.identity);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Color rayColor;
+        
+
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            rayColor = Color.red;
+
+            Ray myray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(myray.origin, myray.direction * 50000000, rayColor);
+            RaycastHit hitInfo;
+
+            ParticleSystem ex = explosion.GetComponent<ParticleSystem>();
+
+            if (Physics.Raycast(myray, out hitInfo))
+            {
+                if (hitInfo.collider.tag == "Crystals")
+                {
+                    Manager.crystals++;
+                    audioSource.PlayOneShot(destroy, 1);
+                    audioSource.PlayOneShot(shot, 1);
+
+                    targets.destroyed();
+                    Explode(hitInfo.transform.position);
+                    Destroy(hitInfo.transform.gameObject);
+                    
+                }    
+            }
+        }
+    }
+}
